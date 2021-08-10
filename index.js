38f5635986e08a26c0061337b1f22b72c6fca2b3
Troy Dorman <troydorman@gmail.com>
2021-08-10 07:20:15 -0600

    set basic get route at slash and included json in our app.use

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
 
+app.use(express.json({extended:false}))
+
 //connect to mongoose
 connectDB();
 
+app.get("/", (req, res) => {
+  res.setEncoding("please use /api/books or /api/genres");
+});
