b020eeba10779e8d8ada18d701484a9b6a68ab56
Troy Dorman <troydorman@gmail.com>
2021-08-10 07:28:07 -0600

    defined our Genre schema

---
+++ models/genre.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+// Genre Schema
+const genreSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  create_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
