da75a4fdd12a376453ce9acde8b3a6fae1b8f9a3
Troy Dorman <troydorman@gmail.com>
2021-08-10 07:22:19 -0600

    booted our server up

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const app = express();
 const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
 
-app.use(express.json({extended:false}))
+app.use(express.json({ extended: false }));
 
 //connect to mongoose
 connectDB();
 
 app.get("/", (req, res) => {
-  res.setEncoding("please use /api/books or /api/genres");
+  res.send("please use /api/books or /api/genres");
 });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
